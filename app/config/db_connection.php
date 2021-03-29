c3d8a5454f9db33f1b40f226cb86faa12c1cdd94
Vitor Martins Cruz <vitormartinscruz.dev@gmail.com>
2021-03-29 17:54:51 -0300

    Change env variables

--- app/config/db_connection.php
+++ app/config/db_connection.php
@@ -10,7 +10,6 @@ abstract class Connection {
             self::$connection = new \PDO('mysql: host=' . getenv('DB_HOST') . '; dbname=' . getenv('DB_NAME'), getenv('DB_USER'), getenv('DB_PASS'));
         }
         catch (\PDOException $e) {
-            echo "\n\naaa\n".getenv('DB_HOST');
             die("It was not possible to connect to the database: " . $e->getMessage());
         }
 
